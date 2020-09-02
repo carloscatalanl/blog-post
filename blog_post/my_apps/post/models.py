2f5a56d6ad56bc50a7127ff72dd00123f5b8e116
Carlos Catalan <carloscatalan94c@gmail.com>
2020-09-02 13:41:19 -0700

    Basic CRUD

---
+++ blog_post/my_apps/post/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+    date = models.DateField(auto_now=True)
+    content = models.CharField(max_length=100)
+
+    def __str__(self):
+        return 'Post id: ' + str(self.id)
+    
---
+++ blog_post/my_apps/post/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
+from .forms import PostForm
+
+class PostListView(ListView):
+    model = Post
+    template_name = "post_list.html"
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "post_create.html"
+    form_class = PostForm
+    success_url = reverse_lazy('post_list')
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = "post_update.html"
+    form_class = PostForm
+    success_url = reverse_lazy('post_list')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy('post_list')
+
+
+
