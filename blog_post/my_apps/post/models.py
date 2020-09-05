a81889f52b245290b43ccab2a8bd67b22d1952a8
Carlos Catalan <carloscatalan94c@gmail.com>
2020-09-04 18:20:21 -0700

    CRUD 2.0"
    "
    2

--- blog_post/my_apps/post/models.py
+++ blog_post/my_apps/post/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     author = models.CharField(max_length=30)
     email = models.EmailField(max_length=254)
     date = models.DateField(auto_now=True)
-    content = models.CharField(max_length=100)
+    content = models.TextField()
 
     def __str__(self):
         return 'Post id: ' + str(self.id)
